da3649dd07044e2be3aedbf928143c1b822002c2
MacBookPro20 <mac20@MacBook-Pro-de-MacBook-3.local>
2023-01-18 21:49:02 -0500

    Portafolio page working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './view/portfolio/portfolio.component';
+import { AboutComponent } from './view/about/about.component';
+import { PrincipalComponent } from './view/principal/principal.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    AboutComponent,
+    PrincipalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/environments/environment.ts
@@ -0,0 +1,39 @@
+export const environment = {
+    name    : "Jurgen"            ,
+    fullName: "Jurgen Sanclemente",
+    mail    : "jurgensanclemente@gmail.com"                                 ,
+    linkedin: "https://www.linkedin.com/in/jurgen-sanclemente-386647220/"   ,
+    github  : "https://github.com/Jurgen212"                                ,
+    aboutLeft   : "Front end developer with more than 2 years of experience developing team projects and work experience for small local businesses. Currently student of systems engineering. Knowledge of Git, Github and CEO."  ,
+    aboutRight  : "The technologies where I specialize are html, css and javascript, with more than a year working with Angular and bootstrap, knowledge of sql and noSql databases such as postgres and MongoDb, some API developments in Node and applications using Firebase for small Projects.Also knowledge of languages such as Python, Java and C++.Currently learning and developing projects in React.", 
+    descripcion : "Front End Developer",
+
+    proyectos: [{
+        nombre      : 'Pomodoro Timer',
+        imagen      : 'todo-app-react.png',
+        description : 'Application with a pomodoro timer for students, customizable times and the option to write Todo`s. Firebase was used for the database and user login. Mainly developed in React. Responsive and has the possibility of installing on the cell phone.',
+        urlInternet : 'https://verdant-beignet-4a6fe7.netlify.app'
+    },{
+        nombre      : 'User Login',
+        imagen      : 'labsoft-login-angular.png',
+        description : 'Application with CRUD of users, login and different possibilities. Developed together with Sebastian Dosman ( https://github.com/SebasDosman ) using angular and firebase for data management. Currently working on the mobile version',
+        urlInternet : 'https://poetic-sorbet-e82274.netlify.app'
+    },{
+        nombre      : 'Gallery App - Angular',
+        imagen      : 'gallery-app-angular.png',
+        description : 'Web application for video and image search from pexels.com, also allows you to view the daily selection of the work team that provides the images. Developed in Angular and Responsive in mobile.',
+        urlInternet : 'https://resplendent-nasturtium-2b40d0.netlify.app'
+    },{
+        nombre      : 'Gallery App - React',
+        imagen      : 'gallery-app-react.png',
+        description : 'Web application to search for images from an api. Developed in React, responsive and has the possibility of installing on the cell phone.',
+        urlInternet : 'https://magenta-otter-1ea779.netlify.app'
+    },{
+        nombre      : 'Heroes App',
+        imagen      : 'heroes-app-react.png',
+        description : 'Web application to search for "local" information from react, route protection practice and also the management of the path in the url. Adapted for small screens.',
+        urlInternet : 'https://imaginative-figolla-39984e.netlify.app'
+    },]
+};
+
+
